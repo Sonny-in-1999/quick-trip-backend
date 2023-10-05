3c3c0eb7b89556c4d605f4383080a1537aaa57c5
minsu <minsoo1022@gmail.com>
2023-10-06 02:27:05 +0900

    .

--- src/main/java/com/neurotoxin/quicktrip/QuickTripApplication.java
+++ src/main/java/com/neurotoxin/quicktrip/QuickTripApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class QuickTripApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(QuickTripApplication.class, args);
 	}
